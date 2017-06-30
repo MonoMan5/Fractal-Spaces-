00d33cf6a588aabe44a1b3b2cb8b591cb4433830
MonoMan5 <amar.sehic@gmail.com>
2017-06-30 03:10:42 -0400

    Update Liquidspace Text Wrangler.py

--- Liquidspace Text Wrangler.py
+++ Liquidspace Text Wrangler.py
@@ -30,6 +30,9 @@ input_file = 'test_amar-2'+'.csv'
 data = pd.read_csv('C:/Users/Amar Sehic/Documents/Fractal/Python Web Scrapping/Liquidspace Raw Text/'+input_file)
 d = data['table']
 d = d[0]
+d = d.replace(u'\xa0', ' ')
+
+remove = Word(alphas) + pp.Literal('This reservation was created in different reservation mode for workspace, please correct.')
 
 date = Word( alphas ) + Word(nums) +'-' + Word( alphas ) + Word(nums) + pp.Suppress(',') + pp.Suppress(Word(nums))
 location = Word( pp.alphanums+'-()#.' )
@@ -40,6 +43,9 @@ prices_loc = price_loc.searchString(d)
 
 week = week_date[0]+ ' ' +week_date[1] + ' ' + week_date[2] + ' ' + week_date[3]+ ' '+week_date[4]
 
+start_day = week_date[1]
+
+
 prices_arr = []
 
 for s in prices_loc:
@@ -48,4 +54,18 @@ for s in prices_loc:
     prices_arr.append((key,value))
     
 
- 
+Days_of_week = ['Mon','Tue','Wed','Thu','Fri','Sat','Sun']
+
+timeslot_arr = []
+
+for ii in range(7):
+    day = int(start_day) +ii
+    
+    timeslot = pp.OneOrMore(Word(nums+':') + Word(alphas) + pp.Suppress('-') + Word(nums+':') + Word(alphas) + pp.Suppress(remove))
+    extra = pp.Or([Word(alphas) + Word(alphas), timeslot, pp.Empty()]) 
+    start = Word(alphas) + pp.Suppress(',') + Word( alphas) + str(day) + extra 
+    
+    dd = start.searchString(d)
+    timeslot_arr.append(dd)
+
+print (timeslot_arr, len(timeslot_arr))
