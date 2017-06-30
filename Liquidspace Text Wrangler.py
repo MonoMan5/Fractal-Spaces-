e7aeb1a4cdebdbb98c2392c8812abe23a5af374c
MonoMan5 <amar.sehic@gmail.com>
2017-06-30 03:44:22 -0400

    Update Liquidspace Text Wrangler.py

--- Liquidspace Text Wrangler.py
+++ Liquidspace Text Wrangler.py
@@ -20,6 +20,62 @@ from pyparsing import Word,alphas,nums
 #   FUNCTIONS
 ###############################################################################
 
+def raw_parse(filename):
+    '''
+    The first function in the parsing pipeline. Takes in the raw block of data
+    from the CSV file and parses the text to return two arrays:
+        
+        week - A string to identify the week
+        prices_arr - Tuples of prices and offices names
+        timeslot_arr - Big array encoding office usage over the week, hit this
+                       with the unwrap function to make sense of it
+        
+    '''
+    input_file = filename
+    data = pd.read_csv('C:/Users/Amar Sehic/Documents/Fractal/Python Web Scrapping/Liquidspace Raw Text/'+input_file)
+    d = data['table']
+    d = d[0]
+    d = d.replace(u'\xa0', ' ')  #Encoding is messed up, 
+                                 #this character \xa0 causes problems if not removed
+    
+    remove = Word(alphas) + pp.Literal('This reservation was created in different reservation mode for workspace, please correct.')
+    
+    date = Word( alphas ) + Word(nums) +'-' + Word( alphas ) + Word(nums) + pp.Suppress(',') + pp.Suppress(Word(nums))
+    location = Word( pp.alphanums+'-()#.' )
+    price_loc = pp.Suppress('$') + Word(nums) + pp.Suppress('/') + Word(alphas) + pp.OneOrMore(location)
+    
+    week_date = date.parseString(d)
+    prices_loc = price_loc.searchString(d)
+    
+    week = week_date[0]+ ' ' +week_date[1] + ' ' + week_date[2] + ' ' + week_date[3]+ ' '+week_date[4]
+    
+    start_day = week_date[1]
+    
+    
+    prices_arr = []   #One of the two arrays returned, returns tuples of price per
+                      # time period and the name of the office
+    
+    for s in prices_loc:
+        key = s[0] +' '+s[1]
+        value = " ".join(s[2:])
+        prices_arr.append((key,value))
+        
+    
+    timeslot_arr = []   #The second array returned, contains a big mess of data,
+                        #basically encodes all the usage info for each office over the week.
+                        #Call the unwrap function on this beast to make sense of it
+    
+    for ii in range(7):
+        day = int(start_day) +ii
+        
+        timeslot = pp.OneOrMore(Word(nums+':') + Word(alphas) + pp.Suppress('-') + Word(nums+':') + Word(alphas) + pp.Suppress(remove))
+        extra = pp.Or([Word(alphas) + Word(alphas), timeslot, pp.Empty()]) 
+        start = Word(alphas) + pp.Suppress(',') + Word( alphas) + str(day) + extra 
+        
+        dd = start.searchString(d)
+        timeslot_arr.append(dd)
+
+    return week, prices_arr,timeslot_arr
 
 
 ###############################################################################
@@ -27,45 +83,5 @@ from pyparsing import Word,alphas,nums
 ###############################################################################
 
 input_file = 'test_amar-2'+'.csv'
-data = pd.read_csv('C:/Users/Amar Sehic/Documents/Fractal/Python Web Scrapping/Liquidspace Raw Text/'+input_file)
-d = data['table']
-d = d[0]
-d = d.replace(u'\xa0', ' ')
 
-remove = Word(alphas) + pp.Literal('This reservation was created in different reservation mode for workspace, please correct.')
-
-date = Word( alphas ) + Word(nums) +'-' + Word( alphas ) + Word(nums) + pp.Suppress(',') + pp.Suppress(Word(nums))
-location = Word( pp.alphanums+'-()#.' )
-price_loc = pp.Suppress('$') + Word(nums) + pp.Suppress('/') + Word(alphas) + pp.OneOrMore(location)
-
-week_date = date.parseString(d)
-prices_loc = price_loc.searchString(d)
-
-week = week_date[0]+ ' ' +week_date[1] + ' ' + week_date[2] + ' ' + week_date[3]+ ' '+week_date[4]
-
-start_day = week_date[1]
-
-
-prices_arr = []
-
-for s in prices_loc:
-    key = s[0] +' '+s[1]
-    value = " ".join(s[2:])
-    prices_arr.append((key,value))
-    
-
-Days_of_week = ['Mon','Tue','Wed','Thu','Fri','Sat','Sun']
-
-timeslot_arr = []
-
-for ii in range(7):
-    day = int(start_day) +ii
-    
-    timeslot = pp.OneOrMore(Word(nums+':') + Word(alphas) + pp.Suppress('-') + Word(nums+':') + Word(alphas) + pp.Suppress(remove))
-    extra = pp.Or([Word(alphas) + Word(alphas), timeslot, pp.Empty()]) 
-    start = Word(alphas) + pp.Suppress(',') + Word( alphas) + str(day) + extra 
-    
-    dd = start.searchString(d)
-    timeslot_arr.append(dd)
-
-print (timeslot_arr, len(timeslot_arr))
+print (raw_parse(input_file))
