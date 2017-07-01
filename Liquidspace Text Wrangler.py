55f9e9ef364c5a9192f147ffadb3c1c234a96e2d
MonoMan5 <amar.sehic@gmail.com>
2017-06-30 20:50:53 -0400

    Update Liquidspace Text Wrangler.py

--- Liquidspace Text Wrangler.py
+++ Liquidspace Text Wrangler.py
@@ -11,6 +11,7 @@ import pyparsing as pp
 from pyparsing import Word,alphas,nums
 import numpy as np
 import time as TT
+import timeit
 
 ###############################################################################
 #   CLASSES
@@ -81,66 +82,91 @@ def raw_parse(filename):
     return week, prices_arr,timeslot_arr
 
 
+def unwrap(input_file):
+    '''
+    Unwraps the complicated processed text file from raw_parse and creates a 
+    dataframe that stores all the week's data.
+    '''
+    offices_arr = []
+    
+    RP = raw_parse(input_file)
+    
+    which_week = RP[0] 
+    prices = RP[1]
+    mess = RP[2]
+    
+    for ii in range(8):
+        week_arr = []
+        for week in mess:
+            day = week[ii]
+            week_arr.append(day)
+        offices_arr.append((prices[ii],week_arr))
+    
+    MESS = offices_arr
+
+#This is the structure of MESS    
+#MESS[office][price_loc/schedule][(price,period,name)/(Sun,Mon...,Sat)][NaN/(day,month,date,status)]
+    
+    price_pd = []
+    hours_pd = []
+    name_pd=[]
+    day_of_week_pd = []
+    month_pd = []
+    day_date_pd = []
+    time_diffs = []
+    
+    
+    for l in range(len(offices_arr)):
+        
+        for k in range(7):
+            price_hour = MESS[l][0]
+            price_pd.append(price_hour[0])
+            hours_pd.append(price_hour[1])
+            name_pd.append(price_hour[2])
+            
+       
+            office = MESS[l]
+            temp_day = office[1][k][0]
+            temp_month = office[1][k][1]
+            temp_date = office[1][k][2]
+            
+            day_of_week_pd.append(temp_day)
+            month_pd.append(temp_month)
+            day_date_pd.append(temp_date)
+    
+       
+            test = list(MESS[l][1][k])
+            if (len(test)<6):
+                time_diffs.append(np.NaN)
+            else:
+                time = test[3:]
+                time_format = [TT.strptime(time[ii]+time[ii+1], "%I:%M%p").tm_hour+(TT.strptime(time[ii]+time[ii+1], "%I:%M%p").tm_min)/60 for ii in range(0,len(time),2)]
+                result = [abs(time_format[ii]-time_format[ii+1]) for ii in range(0,len(time_format),2)]
+                result = sum(result)
+                time_diffs.append(result)
+        
+    d = {'Day': day_of_week_pd,
+         'Date': day_date_pd,
+         'Month': month_pd,
+         'Name': name_pd,
+         'Price': price_pd,
+         'Period': hours_pd,
+         'Time Used': time_diffs}
+    
+    d = pd.DataFrame(d, index = d['Month'])  #Choose index as appropriate
+    
+    return d,which_week
+    
+
 ###############################################################################
 #   MAIN PROGRAM BODY
 ###############################################################################
 
 input_file = 'test_amar-2'+'.csv'
 
-offices_arr = []
+d = unwrap(input_file)
 
-prices = raw_parse(input_file)[1]
-mess = raw_parse(input_file)[2]
-
-for ii in range(8):
-    week_arr = []
-    for week in mess:
-        day = week[ii]
-        week_arr.append(day)
-    offices_arr.append((prices[ii],week_arr))
-
-MESS = offices_arr
-#MESS[office][price_loc/schedule][(price,period,name)/(Sun,Mon...,Sat)][NaN/(day,month,date,status)]
-
-price_pd = []
-hours_pd = []
-name_pd=[]
-day_of_week_pd = []
-month_pd = []
-day_date_pd = []
+print(d)
 
 
-for l in range(len(offices_arr)):
-    for ii in range(7):
-        price_hour = MESS[l][0]
-        price_pd.append(price_hour[0])
-        hours_pd.append(price_hour[1])
-        name_pd.append(price_hour[2])
-        
-for l in range(len(offices_arr)):
-    for k in range(7):
-        office = MESS[l]
-        temp_day = office[1][k][0]
-        temp_month = office[1][k][1]
-        temp_date = office[1][k][2]
-        day_of_week_pd.append(temp_day)
-        month_pd.append(temp_month)
-        day_date_pd.append(temp_date)
-
-time_diffs = []
-
-for l in range(len(offices_arr)):
-    test = list(MESS[l][1][1])
-    if (len(test)<6):
-        time_diffs.append(np.NaN)
-    else:
-        time = test[3:]
-        time_format = [TT.strptime(time[ii]+time[ii+1], "%I:%M%p").tm_hour+(TT.strptime(time[ii]+time[ii+1], "%I:%M%p").tm_min)/60 for ii in range(0,len(time),2)]
-        result = [abs(time_format[ii]-time_format[ii+1]) for ii in range(0,len(time_format),2)]
-        result = sum(result)
-        time_diffs.append(result)
-print (time_diffs)
-    
-
-#print(price_pd,hours_pd,name_pd)
     
